f1b4ef548b260e5c7495a7bb85d6cda9732a4fbd
Bikram <bikram.pradhan@virtualsoftware.info>
2017-02-14 22:23:24 -0500

    test case updated

--- maven-example/multi1/src/test/java/artifactory/test/AppTest.java
+++ maven-example/multi1/src/test/java/artifactory/test/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App start
+ * Unit test for simple App here
  */
 public class AppTest
     extends TestCase
